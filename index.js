3f8431a38b46cc0da9980a0b54239b96b1b3c93a
mashisora <mashisora@outlook.com>
2022-06-22 06:12:08 +0000

    init

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const cheerio = require('cheerio');
+import * as fs from 'fs';
+import * as cheerio from 'cheerio';
 
-const file = fs.readFileSync('Ambiance.wwu', 'utf8');
+const file = fs.readFileSync('xml/Ambiance.wwu', 'utf8');
 const $ = cheerio.load(file, {xmlMode: true});
 
 let txt1 = "<p>Text.</p>";
 let txt2 = $("<p></p>").text("Text.");
 $("AudioObjects").prepend(txt1, txt2);
 
-fs.writeFileSync('output.xml', $.xml());
+fs.writeFileSync('xml/output.xml', $.xml());
